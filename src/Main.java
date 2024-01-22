2754d0059c4dca7659be2a9a51160d163c925512
Ludek Mraz <ludek.mraz@centrum.cz>
2024-01-22 16:39:25 +0100

    ukol splnen

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+
+import java.net.InetAddress;
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String nameAndSurname;
+        LocalDate sellersBirthdate = LocalDate.of(1980, 1, 1);
+        int numberOfContracts = 3;
+        double weightOfSoldCarrots = 10.5;
+        String sellerLocation = "Brno";
+        String vehiclePlateNr = "ABC 1234";
+        double carConsumption = 5.2;
+        String ipAddress = "12.256.30.12";
+
+
+        double averageCarrotsSoldPerContract = weightOfSoldCarrots / numberOfContracts;
+
+        System.out.println("Průměrné množství prodané mrkve na jednu smlouvu je " + averageCarrotsSoldPerContract + " t.");
     }
 }
